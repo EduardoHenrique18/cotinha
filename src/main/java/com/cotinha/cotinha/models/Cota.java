61382d364ee17ca9a34ab5d7b61cd949bbdb56d4
eduardo <eduardo.henrique204@gmail.com>
2021-09-20 23:43:08 -0300

    COT-09: Implementação do swagger no projeto.

--- src/main/java/com/cotinha/cotinha/models/Cota.java
+++ src/main/java/com/cotinha/cotinha/models/Cota.java
@@ -1,10 +1,15 @@
 package com.cotinha.cotinha.models;
 
-import java.io.Serializable;
-import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
--- src/main/java/com/cotinha/cotinha/models/User.java
+++ src/main/java/com/cotinha/cotinha/models/User.java
@@ -1,18 +1,22 @@
 package com.cotinha.cotinha.models;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Table(name = "usuario")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
